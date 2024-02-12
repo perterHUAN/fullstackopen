7a8d4b98f344bc770e49f115227f9566d6b96157
peterhuan <2754741241@qq.com>
2024-02-12 16:56:03 +0800

    2.9 phone book add a search feature

--- part2/phone-book/src/App.js
+++ part2/phone-book/src/App.js
@@ -3,8 +3,12 @@ import React from "react";
 function App() {
   const [newName, setNewName] = React.useState("");
   const [phoneNumber, setPhoneNumber] = React.useState("");
+  const [search, setSearch] = React.useState("");
   const [persons, setPersons] = React.useState([
-    { name: "Arto Hellas", phoneNumber: "123252352" },
+    { name: "Arto Hellas", phoneNumber: "123252352", idx: 1 },
+    { name: "Ada Lovelace", phoneNumber: "123252352", idx: 2 },
+    { name: "Dan Abramov", phoneNumber: "123252352", idx: 3 },
+    { name: "Mary Poppendieck", phoneNumber: "123252352", idx: 4 },
   ]);
   function handleChangeName(event) {
     const value = event.target.value;
@@ -22,9 +26,24 @@ function App() {
     }
     setPersons(persons.concat({ name: newName, phoneNumber: phoneNumber }));
   }
+  function handleSearchChange(event) {
+    const value = event.target.value;
+    setSearch(value);
+  }
+  const reg = new RegExp(search, "i");
+  const searchPersons = persons.filter((person) => reg.test(person.name));
   return (
     <div>
       <h2>Phonebook</h2>
+      <label id="search">filter shown with</label>
+      <input
+        htmlFor="search"
+        type="text"
+        name="search"
+        value={search}
+        onChange={handleSearchChange}
+      ></input>
+      <h2>add a new</h2>
       <form onSubmit={handleAdd}>
         <div>
           <label htmlFor="name">Name</label>
@@ -53,7 +72,7 @@ function App() {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {searchPersons.map((person) => (
         <p key={person.name}>
           {person.name} {person.phoneNumber}
         </p>
