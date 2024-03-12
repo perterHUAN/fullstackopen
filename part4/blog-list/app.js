77e612e8e6d445c37c74d4eedbbee4f229aaa973
perterHUAN <47806541+perterHUAN@users.noreply.github.com>
2024-03-12 21:58:52 +0800

    4.2 blog list refactor into standalone modules

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
 const cors = require("cors");
+const middleware = require("./utils/middleware");
 const blogRouter = require("./controls/blog");
 
 const mongoose = require("mongoose");
@@ -17,7 +18,16 @@ mongoose
 const app = express();
 app.use(cors());
 app.use(express.json());
+// place the middleware below
+// after expresson.jon() to ensure we obtain a valid blog.
+app.use(middleware.requestLogger);
 
 app.use("/api/blogs", blogRouter);
+
+// don't match any path
+app.use(middleware.unknownEndpoint);
+
+// finally, add errorHandle middleware
+app.use(middleware.errorHandler);
 
 module.exports = app;
--- part4/blog-list/controls/blog.js
+++ part4/blog-list/controls/blog.js
@@ -2,18 +2,23 @@ const blogRouter = require("express").Router();
 const Blog = require("../models/blog");
 
 // the path becomes shorter.
-blogRouter.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+blogRouter.get("/", (request, response, next) => {
+  Blog.find({})
+    .then((blogs) => {
+      response.json(blogs);
+    })
+    .catch((error) => next(error));
 });
 
 blogRouter.post("/", (request, response) => {
   const blog = new Blog(request.body);
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
+  blog
+    .save()
+    .then((result) => {
+      response.status(201).json(result);
+    })
+    .catch((error) => next(error));
 });
 
 module.exports = blogRouter;
