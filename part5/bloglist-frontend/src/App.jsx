de6ac17a9bbe92ec42ee360ddea9c29a0c24242d
perterHUAN <47806541+perterHUAN@users.noreply.github.com>
2024-03-18 12:08:20 +0800

    5.4 blog list frontend, step 4 notify users of sucessful or failed opeartions

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -36,7 +36,7 @@ const App = () => {
       setPassword("");
     } catch (exception) {
       // error handle
-      setMessage("Login Fail");
+      setMessage("wrong username or password");
       setTimeout(() => setMessage(""), 5000);
     }
   };
@@ -86,6 +86,8 @@ const App = () => {
         author: "",
         url: "",
       });
+      setMessage(`a new blog ${title} by ${author} added`);
+      setTimeout(() => setMessage(""), 5000);
     } catch (exception) {
       setMessage("Create Blog Fail");
       setTimeout(() => setMessage(""), 5000);
