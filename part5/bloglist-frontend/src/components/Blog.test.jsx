15e0d8b53c0227542c3da5588dc662d4f6c990a6
perterHUAN <47806541+perterHUAN@users.noreply.github.com>
2024-03-19 20:33:58 +0800

    5.14 blog list tests, step 2 add test to verify Blog component rending after clicking "show" button

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
 
 describe("<Blog blog addLikes removeBlog>", () => {
@@ -24,10 +25,32 @@ describe("<Blog blog addLikes removeBlog>", () => {
     });
 
     // check whether both url and likes don't exist
-    const urlElement = screen.queryByText("url");
+    const urlElement = screen.queryByText(blog.url);
     expect(urlElement).toBeNull();
 
     const likesElement = screen.queryByText("likes");
     expect(likesElement).toBeNull();
+  });
+
+  test("after clicking button, display url and likes", async () => {
+    // parameters  blog addLikes removeBlog
+    const blog = {
+      author: "peter",
+      title: "Hello World",
+      url: "https://www.baidu.com",
+      likes: 10,
+    };
+
+    const addLikes = vi.fn();
+    const removeBlog = vi.fn();
+    render(<Blog blog={blog} addLikes={addLikes} removeBlog={removeBlog} />);
+    // click button
+    const showButton = screen.getByText("show");
+    const user = userEvent.setup();
+
+    await user.click(showButton);
+    // check whther url and likes exist
+    screen.getByText(blog.url, { exact: false });
+    screen.getByText("likes", { exact: false });
   });
 });
