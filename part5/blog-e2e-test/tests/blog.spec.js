4d8add69029749175893c8b5428ffe1ece17d39c
perterHUAN <47806541+perterHUAN@users.noreply.github.com>
2024-03-21 19:19:49 +0800

    5.17 Blog List End to End Testing, step1 login form should be displayed first

--- part5/blog-e2e-test/tests/blog.spec.js
+++ part5/blog-e2e-test/tests/blog.spec.js
@@ -1,5 +1,15 @@
 const { test, expect, beforeEach, describe } = require("@playwright/test");
 
 describe("Blog List App", () => {
-  test("should show login form", () => {});
+  beforeEach(async ({ page }) => {
+    await page.goto("http://localhost:5173");
+  });
+  test("Login form is shown", async ({ page }) => {
+    // two checkbox boxes for inputing username or password
+    const password = page.getByRole("textbox", { name: "password" });
+    await expect(password).toBeVisible();
+
+    const username = page.getByRole("textbox", { name: "username" });
+    await expect(username).toBeVisible();
+  });
 });
