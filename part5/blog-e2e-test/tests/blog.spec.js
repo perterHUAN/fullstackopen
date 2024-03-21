8b06e6b411b3fd13fc3e57ba815b30d7236e1436
perterHUAN <47806541+perterHUAN@users.noreply.github.com>
2024-03-21 20:21:10 +0800

    5.18 blog list end to end test, step 2 test both successful and failed login

--- part5/blog-e2e-test/tests/blog.spec.js
+++ part5/blog-e2e-test/tests/blog.spec.js
@@ -1,7 +1,17 @@
 const { test, expect, beforeEach, describe } = require("@playwright/test");
 
 describe("Blog List App", () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    // clear out database
+    await request.post("http://localhost:3003/api/testing/reset");
+    // for test login
+    await request.post("http://localhost:3003/api/users", {
+      data: {
+        name: "peterhuan",
+        username: "peterhuan",
+        password: "123456",
+      },
+    });
     await page.goto("http://localhost:5173");
   });
   test("Login form is shown", async ({ page }) => {
@@ -12,4 +22,42 @@ describe("Blog List App", () => {
     const username = page.getByRole("textbox", { name: "username" });
     await expect(username).toBeVisible();
   });
+
+  describe("Login", () => {
+    test("succeeds with correct credentials", async ({ page }) => {
+      const correctCredential = {
+        username: "peterhuan",
+        password: "123456",
+      };
+
+      await loginWith(page, correctCredential);
+      const message = page.getByText(
+        `${correctCredential.username} logged in`,
+        { exact: false }
+      );
+      expect(message).toBeDefined();
+    });
+    test("failed with wrong credentials", async ({ page }) => {
+      const wrongCredential = {
+        username: "peterhan",
+        password: "09876",
+      };
+
+      await loginWith(page, wrongCredential);
+
+      const message = page.getByText("wrong username or password");
+      expect(message).toBeDefined();
+    });
+  });
 });
+
+async function loginWith(page, credentials) {
+  const password = page.getByRole("textbox", { name: "password" });
+  await password.fill(credentials.password);
+
+  const username = page.getByRole("textbox", { name: "username" });
+  await username.fill(credentials.username);
+
+  const submitButton = page.getByRole("button", { name: "login" });
+  await submitButton.click();
+}
